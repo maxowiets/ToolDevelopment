9132bc960f98ded1e3d46b857d7a2443f9888106
Maxym <maxymebeling@gmail.com>
2023-03-08 12:37:15 +0100

    Extra functionality

--- ToolDevelopment/Assets/UIManager.cs
+++ ToolDevelopment/Assets/UIManager.cs
@@ -2,6 +2,7 @@ using SFB;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -26,22 +27,83 @@ public class UIManager : MonoBehaviour
     public List<Image> checkmarksOutline;
     public Image checkmarkOriginal;
 
+    int currentFrame;
+    int totalFrames;
+    public GameObject animationScrollingButtons;
+    public TextMeshProUGUI animationFrameCounter;
+    public GameObject playButton;
+    public GameObject pauseButton;
+
+    public Slider hueSlider;
+    public Slider saturationSlider;
+    public Slider valueSlider;
+
+    public Slider hueSliderGradient;
+    public Slider saturationSliderGradient;
+    public Slider valueSliderGradient;
+
+    public GameObject swapColorButton;
+
+    float widthMultiplier;
+    float heightMultiplier;
 
     private void Start()
     {
+        totalFrames = 1;
+        currentFrame = 0;
+        animationScrollingButtons.SetActive(false);
         outlineThickness = 1;
-        UpdateThicknessUINumber();
-        Downscale64x64();
         ClearOutline();
+        UpdateThicknessUINumber();
+        NormalColor();
+        Downscale128x128();
     }
 
     public void LoadImage()
     {
         WriteResult(StandaloneFileBrowser.OpenFilePanel("Open File", "", "", false));
         mainTexture = ImageLoader.LoadImage(_path.Replace(@"\", "/").Replace("\n", ""));
+        Debug.Log(mainTexture.width % mainTexture.height);
+        if (mainTexture.width % mainTexture.height == 0)
+        {
+            widthMultiplier = 1;
+            heightMultiplier = 1;
+            SetImageSize();
+            currentFrame = 0;
+            totalFrames = mainTexture.width / mainTexture.height;
+            if (totalFrames > 1)
+            {
+                animationScrollingButtons.SetActive(true);
+            }
+            else
+            {
+                animationScrollingButtons.SetActive(false);
+            }
+            VisualizeAnimationFrame();
+        }
+        else
+        {
+            totalFrames = 1;
+            if (mainTexture.width > mainTexture.height)
+            {
+                widthMultiplier = 1;
+                heightMultiplier = (float)mainTexture.height / (float)mainTexture.width;
+            }
+            else
+            {
+                heightMultiplier = 1;
+                widthMultiplier = (float)mainTexture.width / (float)mainTexture.height;
+            }
+            SetImageSize();
+        }
+
         image.texture = mainTexture;
+        PauseAnimation();
         switch (pixelateMode)
         {
+            case PixelateMode.X128:
+                Downscale128x128();
+                break;
             case PixelateMode.X64:
                 Downscale64x64();
                 break;
@@ -83,6 +145,11 @@ public class UIManager : MonoBehaviour
         _path = path;
     }
 
+    void SetImageSize()
+    {
+        image.GetComponent<RectTransform>().sizeDelta = new Vector2(512 * widthMultiplier, 512 * heightMultiplier);
+    }
+
     public void ToggleOriginalImage()
     {
         if (image.texture != null)
@@ -94,7 +161,7 @@ public class UIManager : MonoBehaviour
             }
             else
             {
-                image.texture = pixelateLayer;
+                image.texture = outlineLayer;
                 checkmarkOriginal.gameObject.SetActive(false);
             }
         }
@@ -102,22 +169,24 @@ public class UIManager : MonoBehaviour
 
     void DownscaleImage(int size)
     {
-        Texture2D newTexture = new Texture2D(size, size);
+        Texture2D newTexture = new Texture2D((int)(size * totalFrames * widthMultiplier), (int)(size * heightMultiplier));
 
         float xScale = mainTexture.width / (float)size;
         float yScale = mainTexture.height / (float)size;
 
-        // Loop through each pixel in the downscaled texture and set its color to the average color of the corresponding 8x8 block of pixels in the original texture
-        for (int y = 0; y < size; y++)
+        for (int i = 0; i < totalFrames; i++)
         {
-            for (int x = 0; x < size; x++)
+            for (int y = 0; y < Mathf.FloorToInt(size * heightMultiplier); y++)
             {
-                Color pixelColor = mainTexture.GetPixel((int)((x + 0.5f) * xScale), (int)((y + 0.5f) * yScale));
-                if (pixelColor.a > 0)
+                for (int x = i; x < Mathf.FloorToInt(size * (i + 1) * widthMultiplier); x++)
                 {
-                    pixelColor.a = 1;
+                    Color pixelColor = mainTexture.GetPixel((int)((x + 0.5f) * xScale / totalFrames), (int)((y + 0.5f) * yScale));
+                    if (pixelColor.a > 0)
+                    {
+                        pixelColor.a = 1;
+                    }
+                    newTexture.SetPixel(x, y, pixelColor, 0);
                 }
-                newTexture.SetPixel(x, y, pixelColor, 0);
             }
         }
 
@@ -130,31 +199,39 @@ public class UIManager : MonoBehaviour
         UpdateColorMode();
     }
 
+    public void Downscale128x128()
+    {
+        pixelateMode = PixelateMode.X128;
+        CheckmarkUIElements(0, checkmarksPixelate);
+        if (image.texture != null) DownscaleImage(128);
+
+    }
+
     public void Downscale64x64()
     {
         pixelateMode = PixelateMode.X64;
-        CheckmarkUIElements(0, checkmarksPixelate);
+        CheckmarkUIElements(1, checkmarksPixelate);
         if (image.texture != null) DownscaleImage(64);
     }
 
     public void Downscale32x32()
     {
         pixelateMode = PixelateMode.X32;
-        CheckmarkUIElements(1, checkmarksPixelate);
+        CheckmarkUIElements(2, checkmarksPixelate);
         if (image.texture != null) DownscaleImage(32);
     }
 
     public void Downscale16x16()
     {
         pixelateMode = PixelateMode.X16;
-        CheckmarkUIElements(2, checkmarksPixelate);
+        CheckmarkUIElements(3, checkmarksPixelate);
         if (image.texture != null) DownscaleImage(16);
     }
 
     public void Downscale8x8()
     {
         pixelateMode = PixelateMode.X8;
-        CheckmarkUIElements(3, checkmarksPixelate);
+        CheckmarkUIElements(4, checkmarksPixelate);
         if (image.texture != null) DownscaleImage(8);
     }
 
@@ -162,31 +239,76 @@ public class UIManager : MonoBehaviour
     {
         colorMode = ColorMode.NORMAL;
         CheckmarkUIElements(0, checkmarksColor);
-        if (image.texture != null) colorLayer = pixelateLayer;
-    }
 
-    public void BlackAndWhite()
-    {
-        colorMode = ColorMode.BLACKANDWHITE;
-        CheckmarkUIElements(1, checkmarksColor);
+        hueSlider.gameObject.SetActive(false);
+        saturationSlider.gameObject.SetActive(false);
+        valueSlider.gameObject.SetActive(false);
+
+        hueSliderGradient.gameObject.SetActive(false);
+        saturationSliderGradient.gameObject.SetActive(false);
+        valueSliderGradient.gameObject.SetActive(false);
+
+        swapColorButton.SetActive(false);
 
         if (image.texture != null)
         {
-            Texture2D newTexture = new Texture2D(pixelateLayer.width, pixelateLayer.height);
-
-            for (int y = 0; y < pixelateLayer.height; y++)
-            {
-                for (int x = 0; x < pixelateLayer.width; x++)
-                {
-                    float grayscaleAmount = pixelateLayer.GetPixel(x, y).grayscale;
-                    newTexture.SetPixel(x, y, new Color(grayscaleAmount, grayscaleAmount, grayscaleAmount, 1), 0);
-                }
-            }
-            newTexture.Apply();
-            newTexture.filterMode = FilterMode.Point;
-            colorLayer = newTexture;
+            colorLayer = pixelateLayer;
             image.texture = colorLayer;
         }
+        UpdateOutline();
+    }
+
+    public void HSVColor()
+    {
+        colorMode = ColorMode.HSV;
+        CheckmarkUIElements(1, checkmarksColor);
+        
+        hueSlider.gameObject.SetActive(true);
+        saturationSlider.gameObject.SetActive(true);
+        valueSlider.gameObject.SetActive(true);
+
+        hueSliderGradient.gameObject.SetActive(false);
+        saturationSliderGradient.gameObject.SetActive(false);
+        valueSliderGradient.gameObject.SetActive(false);
+
+        swapColorButton.SetActive(false);
+
+        ChangeColorFromHSV();
+    }
+
+    public void GradientColor()
+    {
+        colorMode = ColorMode.GRADIENT;
+        CheckmarkUIElements(2, checkmarksColor);
+
+        hueSlider.gameObject.SetActive(true);
+        saturationSlider.gameObject.SetActive(true);
+        valueSlider.gameObject.SetActive(true);
+
+        hueSliderGradient.gameObject.SetActive(true);
+        saturationSliderGradient.gameObject.SetActive(true);
+        valueSliderGradient.gameObject.SetActive(true);
+
+        swapColorButton.SetActive(true);
+
+        ChangeGradientFromHSV();
+    }
+
+    public void SwapColors()
+    {
+        float h = hueSlider.value;
+        float s = saturationSlider.value;
+        float v = valueSlider.value;
+
+        hueSlider.value = hueSliderGradient.value;
+        saturationSlider.value = saturationSliderGradient.value;
+        valueSlider.value = valueSliderGradient.value;
+
+        hueSliderGradient.value = h;
+        saturationSliderGradient.value = s;
+        valueSliderGradient.value = v;
+
+        ChangeGradientFromHSV();
     }
 
     void UpdateColorMode()
@@ -196,13 +318,91 @@ public class UIManager : MonoBehaviour
             case ColorMode.NORMAL:
                 NormalColor();
                 break;
-            case ColorMode.BLACKANDWHITE:
-                BlackAndWhite();
+            case ColorMode.HSV:
+                ChangeColorFromHSV();
                 break;
-            default:
+            case ColorMode.GRADIENT:
+                ChangeGradientFromHSV();
                 break;
         }
+    }
 
+    public void ChangeColorFromHSV()
+    {
+        if (colorMode == ColorMode.GRADIENT)
+        {
+            ChangeGradientFromHSV();
+            return;
+        }
+        if (image.texture != null)
+        {
+            Texture2D newTexture = new Texture2D(pixelateLayer.width, pixelateLayer.height);
+            Color color = Color.HSVToRGB(hueSlider.value, saturationSlider.value, valueSlider.value);
+
+            for (int y = 0; y < pixelateLayer.height; y++)
+            {
+                for (int x = 0; x < pixelateLayer.width; x++)
+                {
+                    if (pixelateLayer.GetPixel(x, y).a == 0)
+                    {
+                        newTexture.SetPixel(x, y, new Color(0, 0, 0, 0), 0);
+                    }
+                    else
+                    {
+                        float grayscaleAmount = pixelateLayer.GetPixel(x, y).grayscale;
+                        newTexture.SetPixel(x, y, new Color(color.r * grayscaleAmount, color.g * grayscaleAmount, color.b * grayscaleAmount, 1f), 0);
+                    }
+                }
+            }
+            newTexture.Apply();
+            newTexture.filterMode = FilterMode.Point;
+            colorLayer = newTexture;
+            image.texture = colorLayer;
+        }
+        UpdateOutline();
+    }
+
+    public void ChangeGradientFromHSV()
+    {
+        colorMode = ColorMode.GRADIENT;
+        CheckmarkUIElements(2, checkmarksColor);
+
+        if (image.texture != null)
+        {
+            Texture2D newTexture = new Texture2D(pixelateLayer.width, pixelateLayer.height);
+            var gradient = new Gradient();
+            var colorKey = new GradientColorKey[2];
+
+            colorKey[0].color = Color.HSVToRGB(hueSlider.value, saturationSlider.value, valueSlider.value);
+            colorKey[0].time = 1.0f;
+
+            colorKey[1].color = Color.HSVToRGB(hueSliderGradient.value, saturationSliderGradient.value, valueSliderGradient.value);
+            colorKey[1].time = 0.3f;
+
+            gradient.colorKeys = colorKey;
+            Color color;
+
+            for (int y = 0; y < pixelateLayer.height; y++)
+            {
+                for (int x = 0; x < pixelateLayer.width; x++)
+                {
+                    if (pixelateLayer.GetPixel(x, y).a == 0)
+                    {
+                        newTexture.SetPixel(x, y, new Color(0, 0, 0, 0), 0);
+                    }
+                    else
+                    {
+                        float grayscaleAmount = pixelateLayer.GetPixel(x, y).grayscale;
+                        color = gradient.Evaluate(grayscaleAmount);
+                        newTexture.SetPixel(x, y, new Color(color.r * grayscaleAmount, color.g * grayscaleAmount, color.b * grayscaleAmount, 1f), 0);
+                    }
+                }
+            }
+            newTexture.Apply();
+            newTexture.filterMode = FilterMode.Point;
+            colorLayer = newTexture;
+            image.texture = colorLayer;
+        }
         UpdateOutline();
     }
 
@@ -210,7 +410,11 @@ public class UIManager : MonoBehaviour
     {
         outlineMode = OutlineMode.NONE;
         CheckmarkUIElements(0, checkmarksOutline);
-        if(image.texture != null) image.texture = pixelateLayer;
+        if (image.texture != null)
+        {
+            outlineLayer = colorLayer;
+            image.texture = outlineLayer;
+        }
     }
 
     public void OutlineOutsideThin()
@@ -232,6 +436,8 @@ public class UIManager : MonoBehaviour
 
         Texture2D currentTexture = colorLayer;
         Texture2D newTexture = new Texture2D(currentTexture.width, currentTexture.height, TextureFormat.RGBA32, false);
+
+        Debug.Log(newTexture.width + "   " + newTexture.height);
 
         for (int x = 0; x < currentTexture.width; x++)
         {
@@ -282,7 +488,8 @@ public class UIManager : MonoBehaviour
 
         newTexture.Apply();
         newTexture.filterMode = FilterMode.Point;
-        image.texture = newTexture;
+        outlineLayer = newTexture;
+        image.texture = outlineLayer;
     }
 
     public void OutlineInsideThin()
@@ -304,6 +511,8 @@ public class UIManager : MonoBehaviour
 
         Texture2D currentTexture = colorLayer;
         Texture2D newTexture = new Texture2D(currentTexture.width, currentTexture.height, TextureFormat.RGBA32, false);
+
+        Debug.Log(newTexture.width + "   " + newTexture.height);
 
         for (int x = 0; x < currentTexture.width; x++)
         {
@@ -354,7 +563,8 @@ public class UIManager : MonoBehaviour
 
         newTexture.Apply();
         newTexture.filterMode = FilterMode.Point;
-        image.texture = newTexture;
+        outlineLayer = newTexture;
+        image.texture = outlineLayer;
     }
 
     void CheckmarkUIElements(int position, List<Image> checkmarkList)
@@ -412,13 +622,65 @@ public class UIManager : MonoBehaviour
             default:
                 break;
         }
+
+        checkmarkOriginal.gameObject.SetActive(false);
+    }
+
+    void VisualizeAnimationFrame()
+    {
+        image.uvRect = new Rect(1f / totalFrames * currentFrame, 0, 1f / totalFrames, 1);
+        animationFrameCounter.text = (currentFrame + 1).ToString();
+    }
+
+    public void NextAnimationFrame()
+    {
+        currentFrame++;
+        if (currentFrame >= totalFrames)
+        {
+            currentFrame = 0;
+        }
+        VisualizeAnimationFrame();
+    }
+
+    public void PreviousAnimationFrame()
+    {
+        currentFrame--;
+        if (currentFrame <= 0)
+        {
+            currentFrame = totalFrames - 1;
+        }
+        VisualizeAnimationFrame();
+    }
+
+    public void PlayAnimation()
+    {
+        playButton.SetActive(false);
+        pauseButton.SetActive(true);
+        StartCoroutine("PlayAnimationCoroutine");
+    }
+
+    public void PauseAnimation()
+    {
+        playButton.SetActive(true);
+        pauseButton.SetActive(false);
+        StopAllCoroutines();
+    }
+
+    IEnumerator PlayAnimationCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.125f);
+            NextAnimationFrame();
+        }
     }
 }
 
 
 public enum PixelateMode
 {
-    X64 = 0,
+    X128 = 0,
+    X64,
     X32,
     X16,
     X8,
@@ -436,5 +698,6 @@ public enum OutlineMode
 public enum ColorMode
 {
     NORMAL = 0,
-    BLACKANDWHITE,
+    HSV,
+    GRADIENT,
 }
